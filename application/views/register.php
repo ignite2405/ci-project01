2b1d25326a3bf7fca8fb430eda950884e8f6eb1a
Dimas Natanael <dimznatanael42@gmail.com>
2016-09-12 12:51:38 +0700

    register page

---
+++ application/views/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+	
+	<table>
+		<tr>
+			<td>Username</td>
+			<td>
+				<input type="text" name="username">
+			</td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td>
+				<input type="password" name="password">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="reset">
+			</td>
+			<td>
+				<input type="submit" value="save">
+			</td>
+
+		</tr>
+	</table>
+
+</body>
+</html>
