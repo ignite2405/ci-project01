4eb594d734190bf3ff9bae62e35b23eb41fe8867
Dimas Natanael <dimznatanael42@gmail.com>
2016-09-12 11:55:34 +0700

    add Home class

---
+++ application/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		echo "this is index";
+	}
+
+	public function alvin()
+	{
+		echo "lalalala";
+	}
+
+}
+
+/* End of file Home.php */
+/* Location: ./application/controllers/Home.php */
